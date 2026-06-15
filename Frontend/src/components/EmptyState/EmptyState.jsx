f405ac4f9c7965b8e1f41fd6f4a357645acfb365
Ajay Gour <ajgour14@gmail.com>
2026-06-15 19:37:59 +0530

    adding state

---
+++ Frontend/src/components/EmptyState/EmptyState.jsx
@@ -0,0 +1,11 @@
+function EmptyState({ message }) {
+
+  return (
+    <div style={{textAlign:"center", marginTop:"40px"}}>
+      <p>{message}</p>
+    </div>
+  );
+
+}
+
+export default EmptyState;
