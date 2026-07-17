a80eb1e6c64db87366570f53102cd0790f458ab9
Ajay Gour <ajgour14@gmail.com>
2026-07-17 22:02:27 +0530

    Initialized API Gateway

---
+++ Backend/Services/gateway/src/main/java/com/citymall/GatewayApplication.java
@@ -0,0 +1,14 @@
+package com.citymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+        System.out.println("GatewayApplication is running Successfully : welcome to citymall");
+	}
+
+}
