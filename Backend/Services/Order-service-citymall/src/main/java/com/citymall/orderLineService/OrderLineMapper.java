d99cf62dfaa9ff0ab0b8cf601b69e01879f3f7c8
Ajay Gour <ajgour14@gmail.com>
2026-07-29 23:28:43 +0530

    modified order mapper

--- Backend/Services/Order-service-citymall/src/main/java/com/citymall/orderLineService/OrderLineMapper.java
+++ Backend/Services/Order-service-citymall/src/main/java/com/citymall/orderLineService/OrderLineMapper.java
@@ -7,14 +7,15 @@ import org.springframework.stereotype.Service;
 public class OrderLineMapper {
     public OrderLine toOrderLine(OrderLineRequest request) {
         return OrderLine.builder()
-//                .id(request.orderId())
                 .productId(request.productId())
+                .productName(request.productName())
+                .price(request.price())
+                .quantity(request.quantity())
                 .order(
                         Order.builder()
                                 .id(request.orderId())
                                 .build()
                 )
-                .quantity(request.quantity())
                 .build();
     }
 
